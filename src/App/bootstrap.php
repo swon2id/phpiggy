f5ada3be49620f2e2858b8330273ad1a9dba3810
swon2id <swon2id@gmail.com>
2024-02-07 11:00:38 +0900

    Initial commit

---
+++ src/App/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Framework\App;
+use App\Controllers\HomeController;
+
+$app = new App();
+
+$app->registerRoute('GET', '/', [HomeController::class, 'home']);
+
+return $app;
